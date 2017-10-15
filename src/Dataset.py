d616fdfeceef89854e920137f13d8172f9802034
poltorres0999 <eichgammer@gmail.com>
2017-10-15 21:56:08 +0200

    DataSet finished
    just created methods for init and printing data

--- src/Dataset.py
+++ src/Dataset.py
@@ -5,10 +5,29 @@ import matplotlib.pyplot
 class MnistDataset:
 
     def __init__(self, folder_path):
+
         data_file = open(folder_path)
         self.data_list = data_file.readlines()
         data_file.close()
 
+    def print_data(self):
 
+        for record in self.data_list :
+            all_values = record.data_list.split(',')
+            current_value = all_values[0]
+            image_array = numpy.asfarray(all_values[1:]).reshape((28,28))
+            matplotlib.pyplot.imshow(image_array,cmap='Greys',interpolation='None')
+
+            print ('Current value: %s' % current_value)
+
+
+    def print_data (self, data_location) :
+
+        all_values = self.data_list[data_location].split(',')
+        current_value = all_values[data_location]
+        image_array = numpy.asfarray(all_values[1:]).reshape((28, 28))
+        matplotlib.pyplot.imshow(image_array, cmap='Greys', interpolation='None')
+
+        print('Current value: %s' % current_value)
 
     def get_length(self): return len(self.data_list)
